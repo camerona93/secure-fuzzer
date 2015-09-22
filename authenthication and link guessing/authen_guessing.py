1962d54a26330d2f8c3333346423669029643879
rjahn124 <jej4249@rit.edu>
2015-09-22 15:13:56 -0400

    Psuedo Code
    Added a brief understanding of what is needed for the link discovery/
    authentication. Syntax is incorrect and code isnt complete

--- authenthication and link guessing/authen_guessing.py
+++ authenthication and link guessing/authen_guessing.py
@@ -1,1 +1,20 @@
 from requests.auth import HTTPBasicAuth
+
+
+def grabRequestOfSite( String url):
+    if (url == 'http://localhost:8000/DPWA'):
+        urlRequest = requests.get(url, auth=('admin','password'))
+    else if (url == 'http://localhost:8000/bigot'):
+        urlRequest = requests.get(url, auth=('admin','password'))
+    else:
+        pass
+    return urlRequest
+
+def linkSaver(String currentUrl, String indexUrl, Array linkArray):
+    if(currentUrl != indexUrl):
+        linkArray.append(currentUrl)
+        
+
+def main():
+    linkArray[String]
+
