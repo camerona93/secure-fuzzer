414315725644f0620eac2c9b8bb65a3b2a45233a
Xcelled <j12@ketelaar.com>
2015-09-22 21:09:02 -0400

    Added main fuzzer program

---
+++ fuzzer.py
@@ -0,0 +1,40 @@
+﻿import argparse, requests
+from pagediscover.guessing import guesser
+
+parser = argparse.ArgumentParser(description='Security fuzzer')
+
+parser.add_argument('command', help='Fuzzer mode. Discover or Test', choices=('discover', 'test'))
+parser.add_argument('url', help='URL to fuzz')
+parser.add_argument('--custom-auth', help='Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa). Optional',
+                    choices=('dvwa',), dest='auth')
+parser.add_argument('--common-words', help='Newline-delimited file of common words to be used in page guessing and input guessing. Required.',
+                    required=True, dest='word_file')
+
+def main():
+    args = parser.parse_args()
+
+    print('Fuzzing ' + args.url + ' ...')
+
+    # set up session
+    print('Creating Session...')
+    s = requests.Session()
+    if args.auth is not None: login(s, args.auth)
+
+    print('Trying to guess additional pages...')
+    with open(args.word_file, 'rU') as wf:
+        found = guesser.guess(s, args.url, wf)
+        for f in found: print(f)
+    
+#end def
+
+
+def login(session, site):
+    if site == 'dvwa':
+        r = session.post('http://127.0.0.1/dvwa/login.php', {'username' : 'admin', 'password' : 'password', 'Login' : 'Login'})
+        assert not r.url.endswith('login.php')
+
+    assert r.status_code == requests.codes.okay
+#end def
+
+if __name__ == '__main__':
+    main()
--- pagediscover/guessing/guesser.py
+++ pagediscover/guessing/guesser.py
@@ -1,23 +1,24 @@
-import requests
+﻿import requests
 
 notfound_codes = (requests.codes.no_response, requests.codes.not_found)
 common_ext = { '', '.html', '.htm', '.xhtml', '.php', '.jsp', '.asp', '.aspx', '.xml', '.pl', '.do', '.cgi', '.txt' }
 
-def guess(base_url, word_list, found=set()):
+def guess(session, base_url, word_list, found=set()):
     if base_url[-1] != '/': base_url += '/'
     for word in word_list:
+        word = word.rstrip()
         for ext in common_ext:
-            live, url = is_live(base_url + word + ext)
+            live, url = is_live(session, base_url + word + ext)
             #if live: print(url + ' ' + str(url in found))
             if live and url not in found: found.add(url)
 
     return found
 #end def
 
-def is_live(url):
+def is_live(session, url):
     "Checks a url for liveness. Returns tuple of reachable, and the URL (in case of redirect)"
     # print('Checking ' + url, end=' ... ')
-    r = requests.get(url)
+    r = session.get(url)
     # print(r.status_code)
     return (r.status_code not in notfound_codes), r.url
 #end def
