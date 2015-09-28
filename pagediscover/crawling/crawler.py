8274084a0d7177d342edfd5727b3300bfcaeccd0
Xcelled <j12@ketelaar.com>
2015-09-28 01:24:55 -0400

    Added support for reqs such as 'only same-host links'

--- pagediscover/crawling/crawler.py
+++ pagediscover/crawling/crawler.py
@@ -1,25 +1,31 @@
-﻿try:
+﻿from urllib.parse import urlparse
+
+try:
     from BeautifulSoup import BeautifulSoup, SoupStrainer
 except ImportError:
     from bs4 import BeautifulSoup, SoupStrainer 
 
 link_re = re.compile(r'href="(.?)"')
 
-def crawl(session, url, results=set()):
+def crawl(session, start_url, results=set()):
+    can_url = urlparse(start_url)
+
     todo = set()
-    todo.add(url)
+    todo.add(can_url.path)
 
     while len(todo) != 0:
         url = todo.pop()
         results.add(url)
-        crawl_page(session, url, results, todo)
+        crawl_page(session, can_url.netloc, url, results, todo)
     
     return results
 
-def crawl_page(session, url, done, todo):
-    req = session.get(url)
+def crawl_page(session, host, path, done, todo):
+    req = session.get(host + url)
     for link in BeautifulSoup(req.text, parse_only=SoupStrainer('a')):
         if link.has_attr('href'):
-            href = link['href']
-            if href not in todo and href not in done:
-                todo.add(href)
+            href = urlparse(link['href'])
+            if href.netloc != host: continue
+
+            if href.path not in todo and href.path not in done:
+                todo.add(href.path)
