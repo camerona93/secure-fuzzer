288a0d633a2f5efa1d7a2db7aeb72ed1627c9292
Xcelled <j12@ketelaar.com>
2015-09-28 00:37:37 -0400

    Changed results to be a set, as instructed

--- pagediscover/crawling/crawler.py
+++ pagediscover/crawling/crawler.py
@@ -2,17 +2,16 @@
 
 link_re = re.compile(r'href="(.?)"')
 
-def crawl(session, url, maxlevel):
+def crawl(session, url, maxlevel, results=set()):
     if (maxlevel == 0):
         return
     req = session.get(url)
-    result = []
     links = link_re.findall(req.text)
     for link in links:
         link = urlparse.urljoin(url, link)
-        result += crawl(link, maxlevel - 1)
+        results = results.union(crawl(link, maxlevel - 1))
 
-    return result
+    return results
 
 stuff = crawl("http://127.0.0.1:8080/bodgeit/", 3)
 print(stuff)
