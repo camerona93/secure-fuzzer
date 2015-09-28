6f11844169f3d495a92d9c5af585f327af234aaf
Xcelled <j12@ketelaar.com>
2015-09-28 00:32:22 -0400

    Corrected code formatting and line spacing

--- pagediscover/crawling/crawler.py
+++ pagediscover/crawling/crawler.py
@@ -18,22 +18,21 @@ import requests
 #    return urlRequest
 
 
-
-def crawl(url,maxlevel):
-    link_re =re.compile(r'href="(.?)"')
-    if (maxlevel==0):
+def crawl(url, maxlevel):
+    link_re = re.compile(r'href="(.?)"')
+    if (maxlevel == 0):
         return
     req = requests.get(url)
-    result=[]
+    result = []
     links = link_re.findall(req.text)
     for link in links:
-        link = urlparse.urljoin(url,link)
-        result += crawl(link, maxlevel-1)
+        link = urlparse.urljoin(url, link)
+        result += crawl(link, maxlevel - 1)
 
     return result
     
     
     
 
-stuff= crawl("http://127.0.0.1:8080/bodgeit/",3)
+stuff = crawl("http://127.0.0.1:8080/bodgeit/", 3)
 print(stuff)
