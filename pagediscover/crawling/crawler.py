d500a20d390dcdc3686a50b8b42cc535b67ea109
Xcelled <j12@ketelaar.com>
2015-09-28 00:55:17 -0400

    Unrolled recursive impl into faster set-based one

--- pagediscover/crawling/crawler.py
+++ pagediscover/crawling/crawler.py
@@ -2,13 +2,21 @@
 
 link_re = re.compile(r'href="(.?)"')
 
-def crawl(session, url, maxlevel, results=set()):
-    if (maxlevel == 0):
-        return
+def crawl(session, url, results=set()):
+    todo = set()
+    todo.add(url)
+
+    while len(todo) != 0:
+        url = todo.pop()
+        results.add(url)
+        crawl_page(session, url, results, todo)
+    
+    return results
+
+def crawl_page(session, url, done, todo):
     req = session.get(url)
     links = link_re.findall(req.text)
     for link in links:
         link = urlparse.urljoin(url, link)
-        results = results.union(crawl(link, maxlevel - 1))
-
-    return results
+        if link not in todo and link not in done:
+            todo.add(link)
